48b8cc86b53d3927bbd0b9cc9fe71f97868790f1
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2016-07-27 20:15:04 +0200

    striped away test files and most unused components

--- dbuggr.js
+++ dbuggr.js
@@ -1,4 +1,1 @@
 import Bundleview from './lib/file-browser/bundleview.js';
-// import TextEditor from './lib/source-code-views/text-editor.js';
-// import BarChart from './lib/barchart.js';
-import AceEditor from './lib/source-code-views/ace-editor.js'
