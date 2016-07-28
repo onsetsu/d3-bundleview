499f6779bef26a52fa84c3ec4bbcaef59eb2ba67
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2016-07-28 17:36:00 +0200

    removed reading files

--- src/client/app/generator.js
+++
@@ -1,7 +0,0 @@
-import {utils} from './../utils.js';
-
-export default function() {
-    "use strict";
-
-    return utils.uuid;
-}
--- src/client/utils.js
+++
@@ -1,5 +0,0 @@
-import uuid from './uuid.js';
-
-export let utils = {
-    uuid: uuid
-};
