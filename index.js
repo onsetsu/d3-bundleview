8a48a0a2cf34bf178362ec8f74bba9eec3a32fc4
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2016-07-31 13:52:15 +0200

    added treemap generator

--- index.js
+++ index.js
@@ -1,59 +1,51 @@
-import { Bundleview } from './lib/bundleview.js';
+import { Bundleview, isLeaf, randomLeaf } from './lib/bundleview.js';
 
-function getRandomLinks(root, numberOfLinks) {
-    var links = [];
-    function randomLeaf(root) {
-        if(!root.children) { return root; }
 
-        var index = parseInt((Math.random() * root.children.length), 10);
-        return randomLeaf(root.children[index]);
-    }
-
-    for(var i = 0; i < numberOfLinks; i++) {
-        var source = randomLeaf(root),
-            target = randomLeaf(root);
-
-        if(source !== target) {
-            links.push({
-                source: source.id,
-                target: target.id
-            });
-        }
-    }
-
-    return links;
-}
-
-function attachIDs(root) {
-    var id = 0;
-    function attachIDThenIterate(node) {
-        node.id = id++;
-        node.children && node.children.forEach(attachIDThenIterate);
-    }
-    attachIDThenIterate(root);
-}
 
 // init it all
+var generatedBundleview = new TreeGenerator(4, 50, 10)
+    .createJSON();
+d3.select('#bundleview-root')
+    .attr("width", 700)
+    .attr("height", 600);
+var bundleview = new Bundleview(generatedBundleview, '#bundleview-root');
+//bundleview.newSizeMetric(d => relative.Number);
+//bundleview.newColorScale(d => d[attr].scale.to.absoluteColor);
+
+// second bundleview
 d3.json("example/flare-compat.json", function(error, root) {
+    function getRandomLinks(root, numberOfLinks) {
+        var links = [];
+
+        for(var i = 0; i < numberOfLinks; i++) {
+            var source = randomLeaf(root),
+                target = randomLeaf(root);
+
+            if(source !== target) {
+                links.push({
+                    source: source.id,
+                    target: target.id
+                });
+            }
+        }
+
+        return links;
+    }
+
+    function attachIDs(root) {
+        var id = 0;
+        function attachIDThenIterate(node) {
+            node.id = id++;
+            node.children && node.children.forEach(attachIDThenIterate);
+        }
+        attachIDThenIterate(root);
+    }
+
     if (error) throw error;
 
-    d3.select('#bundleview-root')
-        .attr("width", 700)
-        .attr("height", 600);
     attachIDs(root);
     new Bundleview({
         nodes: root,
         relations: getRandomLinks(root, 1)
-    }, '#bundleview-root');
-
-    d3.json("example/flare-compat.json", function(error, root) {
-        if (error) throw error;
-
-        attachIDs(root);
-        new Bundleview({
-            nodes: root,
-            relations: getRandomLinks(root, 1)
-        }, '#bundleview-root2');
-    });
-
+    }, '#bundleview-root2');
 });
