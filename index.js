bd33a68c68bbefa5cafa43aebf119b8641bde277
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2016-08-14 12:28:52 +0200

    added simple legend to vis

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 import { Bundleview, isLeaf, randomLeaf } from './lib/bundleview.js';
-import TreeGenerator, {BundleView} from './tree-generator/tree-generator.js';
+import TreeGenerator from './tree-generator/tree-generator.js';
 
-// init it all
-var generatedBundleview = new TreeGenerator(4, 50, 100)
-    .createJSON();
-d3.select('#bundleview-root')
-    .attr("width", 700)
-    .attr("height", 600);
+// randomly generated bundleview
+var generatedBundleview = new TreeGenerator(4, 50, 100).createJSON();
 var bundleview = new Bundleview(generatedBundleview, '#bundleview-root');
-//bundleview.newSizeMetric(d => relative.Number);
-//bundleview.newColorScale(d => d[attr].scale.to.absoluteColor);
 
 // second bundleview
 d3.json("example/flare-compat.json", function(error, root) {
     if (error) throw error;
 
-    //attachIDs(root);
     new Bundleview(root, '#bundleview-root2');
 });
